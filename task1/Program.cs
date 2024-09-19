d325083ce1885deb20d34f1c2ef8049837d962c0
mohamed <mohamed@DESKTOP-POBC0ND>
2024-09-19 17:59:34 +0300

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number between 1 and 100");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"no really!! , {number} is my favotite number");
+               
+        }
+    }
+}
